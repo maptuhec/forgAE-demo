18dec10ff575111771eb6d7c51d40762a2e9f647
Martin Grigorov <maptuhec@gmail.com>
2019-01-22 18:29:02 +0200

    remove console logs

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -22,7 +22,6 @@ const deploy = async (network, privateKey) => {
 	let deployer = new Deployer(network, privateKey)
 
 	let result = await deployer.deploy("./contracts/Billboard.aes", gasLimit, initState = "100")
-	console.log(result);
 };
 
 
